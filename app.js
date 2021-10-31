4e35245feb3137a1af984b642a8488bb55d8594e
rgreenan55 <rjgreenan55@gmail.com>
2021-10-31 10:43:12 -0300

    Adjusted Profile API Endpoint

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
 
+const cors = require('cors');
 const express = require('express');
 const app = express();
 
+app.use(cors({
+  origin: '*'
+}));
+
+const profile = require('./routes/profile.js');
+
+app.use('/profile', profile);
+
 const port = process.env.PORT || 3001;
-const users = require('./routes/users.js');
-
-app.use('/users', users);
-
 app.listen(port, () => console.log('App listening on port', port));
