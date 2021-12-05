921c11ff7607bc71990735bfe1d8eddccb20a0fb
rgreenan55 <rjgreenan55@gmail.com>
2021-12-05 10:27:05 -0400

    Updated achievement and game endpoints

--- routes/achievements.js
+++ routes/achievements.js
@@ -3,24 +3,46 @@ const axios = require('axios');
 const express = require('express');
 const router = express.Router();
 
+// Adds More Detial to Achievements // Pictures, etc
+// https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/
 router.get('/getAchievements', async (req, res, next) => {
   try {
     const { gameid, steamid } = req.query;
     const userStats = await axios.get(`https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v1/?key=${process.env.STEAM_KEY}&steamid=${steamid}&appid=${gameid}&l=english`);
     const globalStats = await axios.get(`https://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v2/?gameid=${gameid}`);
+    const gameData = await axios.get(`https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=${process.env.STEAM_KEY}&appid=${gameid}&l=english`);
 
     const playerAchievements = userStats.data.playerstats.achievements;
     const globalAchievements = globalStats.data.achievementpercentages.achievements;
+    const gameSchema = gameData.data.game.availableGameStats.achievements;
+
+    const achieved = [];
+    const locked = [];
+    let count = 0;
 
     playerAchievements.forEach((achievement, i) => {
       const index = globalAchievements.findIndex(item => item.name === achievement.apiname);
       const global = globalAchievements.splice(index, 1)[0];
-      playerAchievements[i].percent = global.percent;
+      achievement.percent = global.percent;
+
+      const index2 = gameSchema.findIndex(item => item.name === achievement.apiname);
+      const schema = gameSchema.splice(index2, 1)[0];
+      achievement.hidden = schema.hidden; 
+      achievement.icon = schema.icon; 
+      achievement.icongray = schema.icongray; 
+
+      if (achievement.achieved) {
+        achieved.push(achievement);
+        count++;
+      } else {
+        locked.push(achievement);
+      }
     });
 
-    res.json(playerAchievements);
+    const percent = (count / playerAchievements.length * 100).toFixed(2);
+
+    res.json({ percent, achieved, locked });
   } catch (error) {
-    console.log(error);
     next(error);
   }
 });
--- routes/games.js
+++ routes/games.js
@@ -25,4 +25,24 @@ router.get('/ownedGames', async (req, res, next) => {
   }
 });
 
+router.get('/game', async (req, res, next) => {
+  try {
+    const { steamid, gameid} = req.query;
+    const gameData = await axios.get(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${process.env.STEAM_KEY}&steamid=${steamid}&include_appinfo=true&appids_filter[0]=${gameid}`);
+    const newsData = await axios.get(`https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?appid=${gameid}&maxlength=1&count=5`);
+
+    const game = gameData.data.response.games[0];
+    const news = newsData.data.appnews.newsitems;
+
+    res.json({ game, news });
+    
+    // News for App
+    // https://partner.steamgames.com/doc/webapi/ISteamNews
+    // appid, maxlength(0 == all), enddate, count, feeds
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
 module.exports = router;
